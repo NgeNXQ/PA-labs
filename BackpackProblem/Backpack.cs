2f2e9277d28e3200c95c0561673078d988c1d573
NgeNXQ <denis.babich.acc@gmal.com>
2023-01-11 03:42:13 +0200

    uploaded labwork 5

---
+++ BackpackProblem/Backpack.cs
@@ -0,0 +1,166 @@
+﻿namespace BackpackProblem
+{
+    internal sealed class Backpack
+    {
+        public readonly int Capacity;
+
+        public int Weight { get; private set; }
+
+        public int TotalPrice { get; private set; }
+
+        private List<Item> items;
+
+        public Backpack(int capacity)
+        {
+            this.Capacity = capacity;
+            this.items = new List<Item>();
+        }
+
+        public void Add(Item item)
+        {
+            if (item.Weight + this.Weight <= this.Capacity)
+            {
+                items.Add(item);
+                this.Weight += item.Weight;
+                this.TotalPrice += item.Price;
+            }
+        }
+
+        public int Fill(List<Item> items)
+        {
+            int weight = 0;
+            List<Item> selectedItems = new List<Item>();
+            items = items.OrderByDescending(item => item.Nectar).ToList();
+
+            foreach (Item item in items)
+            {
+                if (weight + item.Weight <= this.Capacity)
+                {
+                    weight += item.Weight;
+                    selectedItems.Add(item);
+                    Console.WriteLine($"Був доданий новий предмет. Вага: {item.Weight}. Ціна: {item.Price}");
+                }
+                else
+                    break;
+            }
+
+            return selectedItems.Sum(item => item.Price);
+        }
+
+        public int Fill(List<Item> items, int numberOfScouts, int numberOfForagers, int numberOfFields)
+        {
+            List<Item> scoutedItems;
+            List<Item> appropriateItems = GetAppropriateItems(items);
+
+            while (true)
+            {
+                if (items.Count < 1 || this.Weight > this.Capacity || appropriateItems.Count == this.items.Count)
+                    break;
+
+                SpawnScouts(numberOfFields, numberOfScouts, items, out scoutedItems);
+                SpawnForagers(numberOfForagers, scoutedItems, items, out scoutedItems);
+                scoutedItems = GetItems(scoutedItems, appropriateItems);
+
+                if (scoutedItems.Count != 0)
+                {
+                    this.Weight += CalculateWeight(scoutedItems);
+                    this.items.AddRange(scoutedItems);
+
+                    foreach (Item item in scoutedItems)
+                        Console.WriteLine($"Був доданий новий предмет. Вага: {item.Weight}. Ціна: {item.Price}");
+
+                    items.RemoveAll(item => scoutedItems.Contains(item));
+                }
+            }
+
+            return CalculatePrice(this.items);
+
+            void SpawnScouts(int numberOfFields, int numberOfScouts, List<Item> items, out List<Item> scoutedItems)
+            {
+                scoutedItems = new List<Item>();
+                scoutedItems.AddRange(GetRandomItems(0, items.Count, numberOfScouts, items));
+            }
+
+            void SpawnForagers(int count, List<Item> itemsToVisit, List<Item> items, out List<Item> visitedItems)
+            {
+                visitedItems = new List<Item>();
+                visitedItems.AddRange(itemsToVisit);
+
+                if (count > visitedItems.Count)
+                {
+                    List<Item> otherItems = items.Except(visitedItems).ToList();
+                    visitedItems.AddRange(GetRandomItems(0, otherItems.Count, count - visitedItems.Count, items));
+                }
+            }
+
+            List<Item> GetRandomItems(int min, int max, int numberOfElements, List<Item> items)
+            {
+                Random random = new Random();
+                List<Item> selectedItems = new List<Item>();
+
+                int[] positions = Enumerable.Range(min, max).OrderBy(i => random.Next(min, max)).Take(numberOfElements).ToArray();
+
+                for (int i = 0; i < positions.Length; ++i)
+                    selectedItems.Add(items[positions[i]]);
+
+                return selectedItems;
+            }
+
+            List<Item> GetAppropriateItems(List<Item> items)
+            {
+                int weight = 0;
+                List<Item> appropriateItems = new List<Item>();
+                items = items.OrderByDescending(item => item.Nectar).ToList();
+
+                GetList(items, ref weight);
+
+                List<Item> newItemList = items.Except(appropriateItems).OrderBy(item => item.Weight).OrderBy(item => item.Price).ToList();
+
+                GetList(newItemList, ref weight);
+
+                return appropriateItems;
+
+                void GetList(List<Item> items, ref int weight)
+                {
+                    foreach (Item item in items)
+                    {
+                        if (weight + item.Weight <= this.Capacity)
+                        {
+                            weight += item.Weight;
+                            appropriateItems.Add(item);
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            List<Item> GetItems(List<Item> scoutedItems, List<Item> appropriateItems) => scoutedItems.Intersect(appropriateItems).ToList();
+
+            int CalculateWeight(List<Item> selectedItems) => selectedItems.Sum(item => item.Weight);
+
+            int CalculatePrice(List<Item> selectedItems) => selectedItems.Sum(item => item.Price);
+        }
+    }
+
+    internal sealed class Item
+    {
+        public int Weight { get; }
+
+        public int Price { get; }
+
+        public float Nectar { get => (float)this.Price / this.Weight; }
+
+        public Item(int weight, int price)
+        {
+            this.Weight = weight;
+            this.Price = price;
+        }
+
+        public static Item GenerateItemsParameters(int minWeight, int maxWeight, int minPrice, int maxPrice)
+        {
+            Random random = new Random();
+            return new Item(random.Next(minWeight, maxWeight), random.Next(minPrice, maxPrice));
+        }
+    }
+}
---
+++ BackpackProblem/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace BackpackProblem
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int capacity = InputInt("Введіть значення місткості рюкзака. Значення згідно до варіанту - 500. Введіть значення: ", 0, Int32.MaxValue);
+            int numberOfItems = InputInt("Введіть значення кількості предметів. Значення згідно до варіанту - 100. Введіть значення: ", 0, Int32.MaxValue);
+
+            int minPrice = InputInt("Введіть мінімальну цінність предметів у рюкзаку. Значення згідно до варіанту - 2. Введіть значення: ", 0, Int32.MaxValue);
+            int maxPrice = InputInt("Введіть максимальну цінність предметів у рюкзаку. Значення згідно до варіанту - 30. Введіть значення: ", 0, Int32.MaxValue);
+
+            while (minPrice > maxPrice)
+            {
+                Console.WriteLine("Мінімальна ціна має бути менше максимальної!");
+                minPrice = InputInt("Введіть мінімальну цінність предметів у рюкзаку. Значення згідно до варіанту - 2. Введіть значення: ", 0, Int32.MaxValue);
+                maxPrice = InputInt("Введіть максимальну цінність предметів у рюкзаку. Значення згідно до варіанту - 30. Введіть значення: ", 0, Int32.MaxValue);
+            }
+
+            int minWeight = InputInt("Введіть мінімальну вагу предметів у рюкзаку. Значення згідно до варіанту - 1. Введіть значення: ", 0, Int32.MaxValue);
+            int maxWeight = InputInt("Введіть максимальну вагу предметів у рюкзаку. Значення згідно до варіанту - 20. Введіть значення: ", 0, Int32.MaxValue);
+
+            while (minWeight > maxWeight)
+            {
+                Console.WriteLine("Мінімальна вага має бути менше максимальної!");
+                minWeight = InputInt("Введіть мінімальну вагу предметів у рюкзаку. Значення згідно до варіанту - 1. Введіть значення: ", 0, Int32.MaxValue);
+                maxWeight = InputInt("Введіть максимальну вагу предметів у рюкзаку. Значення згідно до варіанту - 20. Введіть значення: ", 0, Int32.MaxValue);
+            }
+
+            int numberOfScouts = InputInt("Введіть кількість розвідників: ", 0, Int32.MaxValue);
+            int numberOfForagers = InputInt("Введіть кількість фуражирів: ", 0, Int32.MaxValue);
+            int numberOfFields = InputInt("Введіть кількість ділянок для дослілдення: ", 0, Int32.MaxValue);
+
+            Backpack backpack = new Backpack(capacity);
+            List<Item> items = new List<Item>(numberOfItems);
+
+            for (int i = 0; i < numberOfItems; ++i)
+                items.Add(Item.GenerateItemsParameters(minWeight, maxWeight, minPrice, maxPrice));
+
+            Console.WriteLine();
+            Console.WriteLine($"\nМаксимальна ціна стандартним методом: {backpack.Fill(items)}");
+            Console.WriteLine();
+            Console.WriteLine($"\nМаксимальна ціна за допомогою бджолиного алгоритму: {backpack.Fill(items, numberOfScouts, numberOfForagers, numberOfFields)}");
+        }
+
+        private static int InputInt(string text, int min, int max)
+        {
+            int value;
+
+            do
+            {
+                Console.Write(text);
+
+                if (Int32.TryParse(Console.ReadLine(), out value))
+                {
+                    if (value <= min)
+                    {
+                        Console.WriteLine($"Введене значення має бути більше {min}!");
+                        continue;
+                    }
+
+                    if (value >= max)
+                    {
+                        Console.WriteLine($"Введене значення має бути менше {max}!");
+                        continue;
+                    }
+
+                    return value;
+                }
+
+                Console.WriteLine("Введена величина має бути числом!");
+
+            } while (true);
+        }
+    }
+}
